7037de60487e39a22e3d2e1e285b99bac7f9d527
Nicholas Chow <nicholaschowhonkit@gmail.com>
2023-08-08 19:45:18 +0800

    Cleaning up the template pages and the routing
    All functions pass the landing page will now use the reader routes and all page templates have had their pages updated with the new navigation bar

--- routes/reader.js
+++ routes/reader.js
@@ -4,86 +4,33 @@ const assert = require("assert");
 
 //reader homepage
 router.get("/home", (req, res) => {
-  res.render("reader-home", {img:'/assets/images/sam-moghadam-khamseh-yxZSAjyToP4-unsplash.jpg'});
+  res.render("reader-home", {
+    img: "/images/sam-moghadam-khamseh-yxZSAjyToP4-unsplash.jpg",
+  });
 });
 
-// Get request for article page template
-router.get("/article", (req, res) => {
-  const articleId = req.query.id; // Extract the articleId from query parameters
-
-  global.db.get(
-    // Retrieve the current banner settings from the database
-    "SELECT * FROM bannerSettings WHERE id = 1",
-    function (err, bannerSettingsData) {
-      if (err) {
-        console.error(err);
-      }
-      global.db.get(
-        //Retrieve the article data from the publishedArticles table based on the articleId
-        "SELECT * FROM publishedArticles WHERE id = ?",
-        [articleId],
-        function (err, articleData) {
-          if (err) {
-            console.error(err);
-          }
-          global.db.all(
-            //Retrieve the comments for the published Article based on the articleId
-            "SELECT * FROM comments WHERE articleId = ?",
-            [articleId],
-            function (err, commentsData) {
-              if (err) {
-                console.error(err);
-              } else {
-                res.render("article", {
-                  bannerSettingsData,
-                  articleData,
-                  commentsData,
-                });
-              }
-            }
-          );
-        }
-      );
-    }
-  );
+// Get request for recipe page template
+router.get("/recipe", (req, res) => {
+  const recipeId = req.query.id; // Extract the recipeId from query parameters (will prob need this later on)
+  res.render("recipe");
 });
 
-//post requeset for the like button
-router.post("/article/likes/:id", (req, res) => {
-  const articleId = req.params.id; // Extract the articleId from route parameters
-
-  // Update the likes for the article in the publishedArticles table
-  global.db.run(
-    "UPDATE publishedArticles SET likes = likes + 1 WHERE id = ?",
-    [articleId],
-    function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        res.redirect(`/reader/article?id=${articleId}`); //inject value of article id + redirect to update the page likes
-      }
-    }
-  );
+//Get request for settings page template
+router.get("/settings", (req, res) => {
+  // Render the settings.ejs template
+  res.render("settings");
 });
 
-//comment for post submit button req
-router.post("/article/comment/:id", (req, res) => {
-  const articleId = req.params.id; // Extract the articleId from route parameters
-  const commentBody = req.body.commentForm; // Extract the comment body from the request body
+//get request for the create-new-recipe page for an article
+router.get("/create-new-recipe", (req, res) => {
+  const recipeId = req.query.id; // Extract the recipeId from query parameters (will prob need this later on)
+  res.render("create-new-recipe");
+});
 
-  // Insert the comment into the comments table
-  global.db.run(
-    "INSERT INTO comments (commentBody, articleId) VALUES (?, ?)",
-    [commentBody, articleId],
-    function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        // Redirect back to the article page after successful comment submission
-        res.redirect(`/reader/article?id=${articleId}`);
-      }
-    }
-  );
+//get request for the create-new-recipe page for an article
+router.get("/profile", (req, res) => {
+  const recipeId = req.query.id; // Extract the recipeId from query parameters (will prob need this later on)
+  res.render("profile");
 });
 
 module.exports = router;
