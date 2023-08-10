bf1e444db56d0abd00814394cb7db4e2fdee3560
Nicholas Chow <nicholaschowhonkit@gmail.com>
2023-08-10 23:17:16 +0800

    Improvements to navigation and side bar
    improvement to the icons and black overlay on sidebar along with new sidebar animation

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,8 +1,31 @@
 var menuIcon = document.querySelector(".burger-menu");
 var sidebar = document.querySelector(".sidebar");
 var pageContent = document.querySelector(".page-content")
+var pageContent = document.querySelector(".page-content");
+var sideMenuIcon = document.querySelector(".sidebar-burger-menu");
+var blackOverlay = document.querySelector(".black-overlay");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("minimised-sidebar");
     pageContent.classList.toggle("page-no-sidebar");
 }
+
+menuIcon.onclick = function () {
+  sidebar.classList.toggle("minimised-sidebar");
+  pageContent.classList.toggle("page-no-sidebar");
+};
+
+sideMenuIcon.onclick = function () {
+  sidebar.classList.toggle("minimised-sidebar");
+  pageContent.classList.toggle("page-no-sidebar");
+  blackOverlay.style.display = "none";
+};
+
+menuIcon.addEventListener("click", function () {
+  blackOverlay.style.display = "block";
+});
+
+blackOverlay.addEventListener("click", function () {
+  blackOverlay.style.display = "none";
+  sidebar.classList.toggle("minimised-sidebar");
+});
