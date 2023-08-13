1217f4929aa1d7ecfa28d2f7c5a64a62e82bff2b
dizizmi <65617120+dizizmi@users.noreply.github.com>
2023-08-13 17:59:58 +0800

    setting up route to leaderboard

--- routes/reader.js
+++ routes/reader.js
@@ -33,4 +33,11 @@ router.get("/profile", (req, res) => {
   res.render("profile");
 });
 
+//get request for leaderboard
+router.get("/leaderboard", (req, res) => {
+  res.render("leaderboard", {
+    img: "/images/sam-moghadam-khamseh-yxZSAjyToP4-unsplash.jpg",
+  });
+});
+
 module.exports = router;
