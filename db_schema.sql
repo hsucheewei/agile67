4d5cc1cc3270bf9668ae438b9b4046daa32942ad
Nicholas Chow <nicholaschowhonkit@gmail.com>
2023-09-05 16:38:59 +0800

    update schema for post count by user

--- db_schema.sql
+++ db_schema.sql
@@ -19,7 +19,8 @@ CREATE TABLE IF NOT EXISTS users (
     lastName TEXT NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
-    profile_image_Name TEXT DEFAULT 'default_avatar' 
+    profile_image_Name TEXT DEFAULT 'default_avatar',
+    posts INTEGER DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS user_likes (
@@ -40,4 +41,22 @@ CREATE TABLE IF NOT EXISTS user_comments (
     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
 );
 
+-- Create a trigger that updates the 'posts' column when a new recipe is inserted
+CREATE TRIGGER IF NOT EXISTS update_posts_on_insert
+AFTER INSERT ON recipes
+FOR EACH ROW
+BEGIN
+  UPDATE users
+  SET posts = posts + 1
+  WHERE users.id = NEW.user_id;
+END;
 
+-- Create a trigger that updates the 'posts' column when a recipe is deleted
+CREATE TRIGGER IF NOT EXISTS update_posts_on_delete
+AFTER DELETE ON recipes
+FOR EACH ROW
+BEGIN
+  UPDATE users
+  SET posts = posts - 1
+  WHERE users.id = OLD.user_id;
+END;
